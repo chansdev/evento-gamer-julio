deeabdb54331785c00ab119e5d3ccde15c7dc300
Heitor Chans <heitorchans3@gmail.com>
2026-02-23 18:43:50 -0300

    versao 3

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// scroll suave
+const linksInternos = document.querySelectorAll('a[href^="#"');
+
+function scrollSuave(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  const sectionTop = section.offsetTop;
+  window.scrollTo({
+    top: sectionTop,
+    behavior: "smooth",
+  });
+}
+
+linksInternos.forEach((e) => {
+  e.addEventListener("click", scrollSuave);
+});
+
+// menu hamburger
+const menubt = document.querySelector("#menuCheck");
+
+menubt.addEventListener("change", (e) => {
+  document.querySelector("#menu").classList.toggle("ativo");
+});
